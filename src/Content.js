9f094973175cccae1159ff86fb0594191a1654b2
Manasvi Arora <manasviarora28@gmail.com>
2024-01-04 16:47:28 +0530

    useSet

--- src/Content.js
+++ src/Content.js
@@ -1,11 +1,13 @@
-
+import {useState} from 'react';
 
 const Content = () => {
-    const name='Manasvi';
+    const  [name, setName] = useState('Dave');
+
+    
   const nameChangeHandler = () => {
     const names = ['Manasvi','Sanya','Dave'];
     const num = Math.floor(Math.random()*3);
-    return names[num];
+    setName(names[num]);
   }
   const handleClick = () =>{
     console.log('You clicked the button');
@@ -19,8 +21,9 @@ const Content = () => {
   return (
     <main>
         <p onDoubleClick={handleClick}>
-            Hello {nameChangeHandler()}</p> {/* here it is called immediately */}
-        <button onClick={handleClick}>Click</button> {/* this works without brackets */}
+            Hello {name} {/* here it is called immediately */}
+            </p> 
+        <button onClick={nameChangeHandler}>Change Name</button> {/* this works without brackets */}
         <button onClick={() => handleClick2('Dave')}>Click</button>{/*  this isnt called immediately even though it has brackets
         because its inside the anonymous function which is activated on the click of the button */}
         <button onClick={(e) => handleClick3(e)}>Click</button>
